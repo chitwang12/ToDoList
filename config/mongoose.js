77aa143fde9bbe013544307a559c96abcc39992f
Yeh Dil Mange More <48553225+chitwang12@users.noreply.github.com>
2023-05-01 09:22:42 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // connect to the database
-const db = mongoose.connect({{ secrets.MONGODB_URI }}).then(()=>{
+const db = mongoose.connect({{"mongodb+srv://chitwan:Chitwan123@cluster0.1mg6bhi.mongodb.net/?retryWrites=true&w=majority"}}).then(()=>{
     console.log('Successfully Connected to Database');
 }).catch((err)=>{
     console.log('Error in connecting to Db ' , err);
