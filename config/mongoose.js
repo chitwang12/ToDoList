4d3da0698d9dbce52d1c061334ae3c3947bad74e
Yeh Dil Mange More <48553225+chitwang12@users.noreply.github.com>
2023-05-01 07:46:00 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,22 +2,10 @@
 const mongoose = require('mongoose');
 
 // connect to the database
-const db = mongoose.connect("mongodb+srv://chitwan:Chitwan123@cluster0.1mg6bhi.mongodb.net/?retryWrites=true&w=majority").then(()=>{
+const db = mongoose.connect({{ secrets.MONGODB_URI }}).then(()=>{
     console.log('Successfully Connected to Database');
 }).catch((err)=>{
     console.log('Error in connecting to Db ' , err);
 })
-
-// // aquire the connection (to check if it is successful)
-// //const db = mongoose.connection;
-
-// // error
-// db.on('error', console.error.bind(console, "Error in connecting to MongoDB"));
-
-// // up and running then print the message
-// db.once('open', function(){
-//     console.log('Connected to Database');
-// });
-
 // exporting the database
 module.exports = db;
